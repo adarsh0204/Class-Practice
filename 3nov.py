a290eb137e64f8d4b73371bc296f36246453de94
Adarsh Verma <112921040+adarsh0204@users.noreply.github.com>
2022-12-07 14:49:48 +0530

    Add files via upload

---
+++ 3nov.py
@@ -0,0 +1,28 @@
+#swapping first and last character
+'''a="Hello"
+print(a[-1]+a[1:-1]+a[0])'''
+'''
+s="hello how are you"
+print("capatilize")
+print(s.capitalize())
+print("lower")
+print(s.lower())
+print("upper")
+print(s.upper())
+print("title")
+print(s.title())
+s1="hello abc"
+print("swapcase")
+print(s.swapcase())
+print("replace")
+s2=s.replace('hello', 'hey')
+print(s2)'''
+'''
+s="python programming"
+print(s.endswith("programming"))
+print(s.startswith("programming"))
+print(s.find("prog"))
+print(s.find("java"))
+print(s.find('0'))
+print(s.rfind('o'))
+print(s.count('m'))'''
